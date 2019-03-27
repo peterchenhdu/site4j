057ccacc4382be95adb133897ec2a9bd159c67f4
cp_hdu@163.com <cp_hdu@163.com>
2019-03-27 23:01:15 +0800

    update js file

--- site4j-web/src/main/resources/static/js/zyd.table.js
+++ site4j-web/src/main/resources/static/js/zyd.table.js
@@ -20,15 +20,13 @@
                     pageNumber: 1,                       //初始化加载第一页，默认第一页
                     pageSize: 10,                       //每页的记录行数（*）
                     pageList: [20, 40, 50, 100, 150],        //可供选择的每页的行数（*）
-                    // search: true,                       //是否启用搜索框 根据sidePagination选择从前后台搜索
-                    // strictSearch: true,                 //设置为 true启用 全匹配搜索，否则为模糊搜索
-                    // searchOnEnterKey: true,            // 设置为 true时，按回车触发搜索方法，否则自动触发搜索方法
                     minimumCountColumns: 1,             //最少允许的列数
                     showColumns: true,                  //是否显示 内容列下拉框
                     showRefresh: true,                  //是否显示刷新按钮
                     showToggle: true,                   //是否显示详细视图和列表视图的切换按钮
                     onExpandRow: options.onExpandRow,
                     rowStyle: options.rowStyle || function (row, index) {
+                        console.log("row:" + row + ", index:" + index);
                         return {};
                     },
                     queryParams: options.queryParams,
@@ -43,7 +41,7 @@
         buttonUtil: {
             init: function (options) {
 
-                if(options.getTreeUrl !==null) {
+                if (options.getTreeUrl !== null) {
                     $.ajax({
                         async: false,
                         type: "POST",
@@ -78,48 +76,58 @@
                 }
 
 
-
-                $("#btn_query").bind("click",function(){
+                $("#btn_query").bind("click", function () {
                     $("#table-list").bootstrapTable('refresh');
                 });
 
                 /* 添加 */
                 $("#btn_add").click(function () {
                     resetForm();
-                    $("#addOrUpdateModal").modal('show');
-                    $("#addOrUpdateModal").find(".modal-dialog .modal-content .modal-header h4.modal-title").html("添加" + options.modalName);
 
-                    if ($("#password") && $("#password")[0]) {
-                        $("#password").attr("required", "required");
+                    var $addOrUpdateModal = $("#addOrUpdateModal");
+                    $addOrUpdateModal.modal('show');
+                    $addOrUpdateModal.find(".modal-dialog .modal-content .modal-header h4.modal-title").html("添加" + options.modalName);
+
+                    var $password = $("#password");
+                    if ($password && $password[0]) {
+                        $password.attr("required", "required");
                     }
-                    if ($("#username") && $("#username")[0]) {
-                        $("#username").removeAttr("readonly");
+
+                    var $username = $("#username");
+                    if ($username && $username[0]) {
+                        $username.removeAttr("readonly");
                     }
+
                     bindSaveInfoEvent(options.createUrl);
                 });
 
+                var $tableList = $('#table-list');
+
                 /* 修改 */
-                $('#table-list').on('click', '.btn-update', function () {
+                $tableList.on('click', '.btn-update', function () {
                     var $this = $(this);
                     var userId = $this.attr("data-id");
                     $.ajax({
                         type: "post",
                         url: options.getInfoUrl.replace("{id}", userId),
                         success: function (json) {
-                            var info = json.data;
-                            // console.log(info);
-                            resetForm(info, options);
-                            $("#addOrUpdateModal").modal('show');
-                            $("#addOrUpdateModal").find(".modal-dialog .modal-content .modal-header h4.modal-title").html("修改" + options.modalName);
-                            if ($("#password") && $("#password")[0]) {
-                                $("#password").removeAttr("required");
+                            resetForm(json.data, options);
+
+                            var $addOrUpdateModal = $("#addOrUpdateModal");
+                            $addOrUpdateModal.modal('show');
+                            $addOrUpdateModal.find(".modal-dialog .modal-content .modal-header h4.modal-title").html("修改" + options.modalName);
+
+                            var $password = $("#password");
+                            if ($password && $password[0]) {
+                                $password.removeAttr("required");
                             }
-                            if ($("#username") && $("#username")[0]) {
-                                $("#username").attr("readonly", "readonly");
+
+                            var $username = $("#username");
+                            if ($username && $username[0]) {
+                                $username.attr("readonly", "readonly");
                             }
 
                             bindSaveInfoEvent(options.updateUrl);
-
                         },
                         error: $.alert.ajaxError
                     });
@@ -155,7 +163,7 @@
                 });
 
                 /* 删除 */
-                $('#table-list').on('click', '.btn-remove', function () {
+                $tableList.on('click', '.btn-remove', function () {
                     var $this = $(this);
                     var userId = $this.attr("data-id");
                     $.alert.confirm("确定删除该" + options.modalName + "信息？", function () {
@@ -181,13 +189,16 @@
 })(jQuery);
 
 function bindSaveInfoEvent(url) {
-    $(".addOrUpdateBtn").unbind('click');
-    $(".addOrUpdateBtn").click(function () {
-        if (validator.checkAll($("#addOrUpdateForm"))) {
+    var $addOrUpdateBtn = $(".addOrUpdateBtn");
+    $addOrUpdateBtn.unbind('click');
+    $addOrUpdateBtn.click(function () {
+
+        var $addOrUpdateForm = $("#addOrUpdateForm");
+        if (validator.checkAll($addOrUpdateForm)) {
             $.ajax({
                 type: "post",
                 url: url,
-                data: $("#addOrUpdateForm").serialize(),
+                data: $addOrUpdateForm.serialize(),
                 success: function (json) {
                     $.alert.ajaxSuccess(json);
                     $("#addOrUpdateModal").modal('hide');
@@ -224,7 +235,7 @@ function clearText($this, type, info, options) {
                 thisValue = 0;
             }
             $this.val(thisValue);
-        } else if(type === "hidden" && $this.next().attr('class') === "ztree") {
+        } else if (type === "hidden" && $this.next().attr('class') === "ztree") {
             $.ajax({
                 async: false,
                 type: "POST",
@@ -247,18 +258,18 @@ function clearText($this, type, info, options) {
                         },
                         callback: {
                             onCheck: function (event, treeId, treeNode) {
-                                console.log(treeNode.id+","+treeNode.tId + ", " + treeNode.name + "," + treeNode.checked);
+                                console.log(treeNode.id + "," + treeNode.tId + ", " + treeNode.name + "," + treeNode.checked);
                                 $this.val(treeNode.id);
 
                             }
                         }
                     };
                     var tree = $.fn.zTree.init($("#treeDemo"), setting, data);
-                    if(thisValue===null) {
+                    if (thisValue === null) {
                         tree.checkAllNodes(false);
                     } else {
-                        tree.checkNode(tree.getNodeByParam("id",thisValue));
-                        tree.expandNode(tree.getNodeByParam("id",thisValue).getParentNode());
+                        tree.checkNode(tree.getNodeByParam("id", thisValue));
+                        tree.expandNode(tree.getNodeByParam("id", thisValue).getParentNode());
                     }
 
                 }
@@ -286,17 +297,4 @@ function getSelectedId() {
         ids.push(selectedJson[i].id);
     });
     return ids;
-}
-
-/**
- * 获取选中的记录
- * @returns {*|jQuery}
- */
-function getSelectedObj() {
-    return $("#table-list").bootstrapTable('getAllSelections');
-}
-
-
-function getBtnString(permissionName, row) {
-    return '<@shiro.hasPermission name="'+permissionName+'"><a class="btn btn-xs btn-primary btn-update" data-id="' + row.id + '"><span class="glyphicon glyphicon-pencil"></span></a></@shiro.hasPermission>';
 }
